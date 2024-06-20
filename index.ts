afb04933dd5980771d5f22751f840ec63e9116b9
GabriellViictor <gabrielvictorgt129@gmail.com>
2024-06-20 11:30:23 -0300

    update2

--- index.ts
+++ index.ts
@@ -1,17 +1,16 @@
-const express = require('express');
+import express, { Request, Response } from 'express';
+
 const app = express();
 
-app.use(express.json()); // Para parsear o corpo das requisições JSON
-
-app.get('/', (request, response) => {
+app.get('/', (request: Request, response: Response) => {
     return response.json({ message: "Servidor funcionando" });
 });
 
-app.post('/teste', (request, response) => {
+app.post('/teste', (request: Request, response: Response) => {
     const { name, date } = request.body;
     return response.json({ name, date });
 });
 
 app.listen(3333, () => {
-    console.log('Servidor está executando na porta 3333');
+    console.log('Servidor rodando na porta 3333');
 });
