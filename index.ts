5c15bed7375195e4e837515787316c203c8c33c7
Gabriel <gabrielvcitorgt129@gmail.com>
2024-06-20 22:45:50 -0300

    add horarios

--- index.ts
+++ index.ts
@@ -7,7 +7,10 @@ const PORT = 3333;
 
 app.use(bodyParser.json());
 
-mongoose.connect('mongodb://localhost:27017/minha_base_de_dados');
+mongoose.connect('mongodb://localhost:27017/minha_base_de_dados', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
 
 const UserSchema = new mongoose.Schema({
     login: String,
@@ -15,6 +18,13 @@ const UserSchema = new mongoose.Schema({
 });
 
 const UserModel = mongoose.model('User', UserSchema);
+
+const HorarioSchema = new mongoose.Schema({
+    horario: String,
+    disponivel: Boolean,
+});
+
+const HorarioModel = mongoose.model('Horario', HorarioSchema);
 
 app.post('/login', async (req: Request, res: Response) => {
     const { login, senha } = req.body;
@@ -30,6 +40,17 @@ app.post('/login', async (req: Request, res: Response) => {
     } catch (err) {
         console.error('Erro ao processar login:', err);
         res.status(500).json({ message: 'Erro ao processar login' });
+    }
+});
+
+
+app.get('/horarios-disponiveis', async (req: Request, res: Response) => {
+    try {
+        const horariosDisponiveis = await HorarioModel.find({ disponivel: true });
+        res.json(horariosDisponiveis);
+    } catch (err) {
+        console.error('Erro ao buscar horários disponíveis:', err);
+        res.status(500).json({ message: 'Erro ao buscar horários disponíveis' });
     }
 });
 
